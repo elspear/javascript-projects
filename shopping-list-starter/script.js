4f4de2f0574c927f64b83d3f7ba4c90ee1d9d531
Emma Spear <emmspear@gmail.com>
2025-09-28 15:15:14 +0800

    completed shopping list

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,7 +1,26 @@
-const addItem = () => {
+let shoppingListItems = [];
 
-};
+const addItem = () => {
+    let item = document.getElementById("new-item-text").value;
+    shoppingListItems = [...shoppingListItems, item];
+    document.getElementById("new-item-text").value = ""; // Clear the input field
+    updateItems();
+}
 
 const updateItems = () => {
+    let listElement = document.getElementById("shopping-list-items");
+    listElement.innerHTML = "";
 
+    for (const shoppingItem of shoppingListItems) {
+        let itemElement = document.createElement("li");
+        itemElement.innerText = shoppingItem;
+        listElement.appendChild(itemElement);
+    }
 };
+
+updateItems();
+
+const clearList = () => {
+    shoppingListItems = [];
+    updateItems();
+}
